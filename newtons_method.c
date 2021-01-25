b32dfab36b17a66069cf30d5af85a4052dea21e8
Motomi Ikeda <ikdmtm01@gmail.com>
2021-01-25 20:36:44 +0900

    add description

--- newtons_method.c
+++ newtons_method.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
 #define PI 3.141592
 
 double get_x1(double x2, double x3)
@@ -18,29 +17,29 @@ int main(void)
 	double x, eps, delta, y, y1;
 	
 	printf("x = ");
-	scanf("%lf", &x);
+	scanf("%lf", &x); //ｘの初期値を代入
 	printf("n_max = ");
-	scanf("%d", &n_max);
+	scanf("%d", &n_max); //繰り返し回数を指定
 	printf("eps = ");
-	scanf("%lf", &eps);
+	scanf("%lf", &eps); //真値との差を指定
 	
 	
 	do 
 	{
 		//y = x*x - x - 6;
 		//y1 = 2*x - 1;
-		y = -x*x + 2*x + 8;
-		y1 = -2*x + 2;
-		delta = y / y1 * -1;
+		y = -x*x + 2*x + 8; //関数を設定
+		y1 = -2*x + 2; //一階微分を設定
+		delta = y / y1 * -1; //deltaの計算
 			
 		printf("delta = %lf\n", delta);
 		x = x + delta;
 		printf("n = %3d, x = %lf\n", n, x);
 		n++;
-	}while(n <= n_max && delta > eps | delta < -eps);
+	}while(n <= n_max && delta > eps | delta < -eps); //指定した真値との差以下になるまで、または指定した繰り返し回数になるまで繰り返す
 	if(n == n_max)
 	{
-		printf("fault\n");
+		printf("fault\n"); //指定した回数で真値に近づかなかったらfaultと表示
 	}else
 	{
 		printf("answer x = %lf\n", x);
